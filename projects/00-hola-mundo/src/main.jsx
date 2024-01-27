620c954e3039187615edb507069aebd324505414
Alberto Guzman <albertpatrsu@gmail.com>
2024-01-26 18:43:22 -0600

    Create app file

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -3,15 +3,6 @@ import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const Button = ({ text }) => {
-  return (
-    <button>{text}</button>
-  )
-}
 root.render(
-  <React.Fragment>
-    <Button text="Me gusta 0" />
-    <Button text="Me gusta 1" />
-    <Button text="Me gusta 2" />
-  </React.Fragment>,
+  <div>Twitter Card</div>  
 );
