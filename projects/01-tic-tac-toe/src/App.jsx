a0992a14c9fc4fe8e6142cc09b47c351b02e2da0
Alberto Guzman <albertpatrsu@gmail.com>
2024-02-08 21:14:58 -0600

    Tic tac toe complited

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -1,35 +1,12 @@
 import { useState } from 'react'
 
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
+import { Square } from './components/Square'
+import { TURNS } from './constants'
+import { checkWinner, checkEndGame } from './board'
+import { WinnerModal } from './components/Winner'
 
-const WINNER_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-]
+import confetti from 'canvas-confetti'
 
-const Square = ( { children, isSelected, updateBoard, index } ) => {
-  
-  const className = `square ${ isSelected ? 'is-selected' : ''}`;
-
-  const handleClick = () => {
-    updateBoard(index);  
-  }
-
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-}
 
 function App() {
 
@@ -37,20 +14,10 @@ function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
 
-  const checkWinner = (boardToCheck) => {
-    
-    for(const combo of WINNER_COMBOS) {
-      const [a, b, c] = combo
-
-      if(
-        boardToCheck[a] &&
-        boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[b] === boardToCheck[c]
-      ) {
-        return boardToCheck[a];
-      }
-    }
-    return null;
+  const resetGame = () => {
+    setWinner(null);
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
   }
 
   const updateBoard = (index) => {
@@ -66,15 +33,17 @@ function App() {
     const newWinner = checkWinner(newBoard);
 
     if(newWinner) {
+      confetti();
       setWinner(newWinner);
+    } else if(checkEndGame(newBoard)) {
+      setWinner(false);
     }
-
-
   }
   
   return (
     <main className="board" >
       <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame} >Resetear el juego</button>
       <section className="game" >
         {
           board.map((_, index) => {
@@ -94,6 +63,8 @@ function App() {
         <Square isSelected={ turn === TURNS.X }> { TURNS.X } </Square>
         <Square isSelected={ turn === TURNS.O }> { TURNS.O } </Square>
       </section>
+      <WinnerModal winner={winner} resetGame={resetGame} />
+      
     </main>
   )
 }
